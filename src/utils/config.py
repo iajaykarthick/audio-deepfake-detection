5151b8919c43403afae19f0d2e1a0cb015ce87e7
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-08-11 10:27:01 -0400

    add utils module

--- src/utils/config.py
+++ src/utils/config.py
@@ -9,6 +9,19 @@ def find_project_root(current_path: Path) -> Path:
             return parent
     return current_path
 
+def resolve_paths(config: dict, root: Path) -> dict:
+    """
+    Recursively resolve paths in the configuration dictionary.
+    """
+    resolved_config = {}
+    for key, value in config.items():
+        if isinstance(value, dict):
+            resolved_config[key] = resolve_paths(value, root)
+        else:
+            full_path = root / value
+            os.makedirs(full_path, exist_ok=True)
+            resolved_config[key] = str(full_path)
+    return resolved_config
 
 def load_config() -> dict:
     project_root = find_project_root(Path(__file__).resolve().parent)
@@ -17,9 +30,7 @@ def load_config() -> dict:
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     
-    for key, path in config.get('data_paths', {}).items():
-        full_path = project_root / path
-        os.makedirs(full_path, exist_ok=True)  
-        config['data_paths'][key] = str(full_path)
-
-    return config    
+    if 'data_paths' in config:
+        config['data_paths'] = resolve_paths(config['data_paths'], project_root)
+    
+    return config
